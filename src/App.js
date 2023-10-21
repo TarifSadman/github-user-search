c98751dcaddf19a69dc35a0ca3fa197e27b80bf7
TarifSadman <sadmantaha17@gmail.com>
2023-10-21 12:18:55 +0600

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { Input, Button, Row, Col, Card, Avatar, Menu } from "antd";
+import { Header } from "antd/es/layout/layout";
 
-function App() {
+
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState([]);
+
+  const getUserData = async () => {
+    try {
+      const response = await axios.get(`https://api.github.com/search/users?q=${username}`);
+      setUserData(response.data.items);
+    } catch (error) {
+      console.error("Error fetching user data: ", error);
+    }
+  };
+
+  const openGitHubProfile = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Header>
+
+      <div style={{ padding: "20px" }}>
+        <Row gutter={16}>
+          <Col span={18}>
+            <Input
+              placeholder="Enter a GitHub username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Col>
+          <Col span={6}>
+            <Button type="primary" onClick={getUserData}>
+              Search
+            </Button>
+          </Col>
+        </Row>
+        <div style={{ marginTop: "20px" }}>
+          {userData.length > 0 ? (
+            userData.map((user) => (
+              <Card key={user.id}>
+                <Card.Meta
+                  avatar={<Avatar src={user.avatar_url} />}
+                  title={user.login}
+                  description={`Name: ${user.login}`}
+                />
+                <p>Number of Repositories: {user.public_repos}</p>
+                <Button
+                  type="primary"
+                  onClick={() => openGitHubProfile(user.html_url)}
+                >
+                  Open GitHub Profile
+                </Button>
+              </Card>
+            ))
+          ) : (
+            <p>No matching users found.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
